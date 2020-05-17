33bd8e8727ae5b6605b6e4b20c888e54a439a224
Alper Arabacı <melihalper.arabaci@gmail.com>
2020-05-17 16:48:01 +0300

    Advanced redux initial Redux DevTools Extension integration

---
+++ react-chapter16-redux-advanced/src/containers/Counter/Counter.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+import CounterControl from '../../components/CounterControl/CounterControl';
+import CounterOutput from '../../components/CounterOutput/CounterOutput';
+
+import { connect } from 'react-redux'
+import * as actionTypes from '../../store/actions/actions';
+
+
+class Counter extends Component {    
+    /*
+    Bu bi ise yaramıyor alttakiler kullanılıyor.
+    state = {
+        counter: 0,
+        results: []
+    }
+    */
+
+    render () {
+        return (
+            <div>
+                <CounterOutput value={this.props.ctr} />
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter.bind(this, 1)} />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter.bind(this, 1)}  />
+                <CounterControl label="Add 5" clicked={this.props.onIncrementCounter.bind(this, 5)} />
+                <CounterControl label="Subtract 5" clicked={this.props.onDecrementCounter.bind(this, 5)}  />
+                <hr />
+                <button onClick={() => this.props.onStoreResult(this.props.ctr)}>Store Result</button>
+                <ul>
+                    <li>{this.props.storedResults.map((result, index) => {
+                            return  <li key={result.id} onClick={this.props.onDeleteResult.bind(this, index)}>{result.value}</li>        
+                    })}</li>
+                               
+                </ul>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        ctr: state.ctr.counter,
+        storedResults: state.res.results
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onIncrementCounter: (value) => dispatch({type: actionTypes.INCREMENT, value: value}),
+        onDecrementCounter: (value) => dispatch({type: actionTypes.DECREMENT, value: value}),    
+        onStoreResult: (result) => dispatch({type:'STORE_RESULT', result: result}),
+        onDeleteResult: (index) => dispatch({type:'DELETE_RESULT', index: index})
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
---
+++ react-chapter16-redux-advanced/src/index.js
@@ -0,0 +1,59 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import counterReducer from './store/reducers/counter';
+import resultReducer from './store/reducers/result';
+import {Provider} from 'react-redux';
+
+import registerServiceWorker from "./registerServiceWorker";
+
+
+/**
+ * Bu nasil kod ya?!? Saglam sovdum onu diyim!
+ */
+const logger = state => {
+
+  return next => {
+      
+      return action => {
+          console.log('[Middleware] Dispatching', action);
+          const result = next(action);
+          console.log('[Middleware] next state', store2.getState());
+          return result;
+      }
+  }
+}
+
+const rootReducer = combineReducers({
+  ctr: counterReducer,
+  res: resultReducer
+});
+
+/**
+ *  Redux DevTools Extension
+ *  http://extension.remotedev.io/#12-advanced-store-setup
+ */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+/**
+ * burada birden fazla middleware olusturulabiliyormuş 
+ * applyMiddleware(logger, logger2, vs..)
+ */
+const store2 = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)));
+
+/**
+ * witout redux devtools extension
+ */
+ //const store2 = createStore(rootReducer, applyMiddleware(logger));
+
+
+
+ReactDOM.render( 
+    <Provider store={store2}>
+    <App />
+    </Provider>,
+  document.getElementById("root")
+);
+registerServiceWorker();
